31f8986e47cdc85d366475ebe672e62350ff35ee
dheaapriandi <dheaapriandi@gmail.com>
2016-04-30 12:27:44 +0700

    Rename g4s to a66a

---
+++ a66a-vendor.mk
@@ -0,0 +1,9 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/evercoss/a66a/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/evercoss/a66a/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/evercoss/a66a/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/evercoss/a66a/proprietary/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/evercoss/a66a/proprietary/vendor,system/vendor)
--- g4s-vendor.mk
+++
@@ -1,9 +0,0 @@
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/jiayu/g4s/proprietary/bin,system/bin)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/jiayu/g4s/proprietary/etc,system/etc)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/jiayu/g4s/proprietary/lib,system/lib)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/jiayu/g4s/proprietary/xbin,system/xbin)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/jiayu/g4s/proprietary/vendor,system/vendor)
